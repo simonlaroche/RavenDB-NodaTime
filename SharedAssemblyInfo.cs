23b56b1191a7b51fc7de03e4e8b670641e49a33b
Simon Laroche <laroche.simon@gmail.com>
2017-01-21 13:01:12 -0500

    update assmbly version

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -5,5 +5,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyProduct("Noda Time Support for RavenDB")]
 [assembly: AssemblyCopyright("Copyright © 2012-2015 Matt Johnson")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("3.0.0.*")]
-[assembly: AssemblyInformationalVersion("3.0.0")]
+[assembly: AssemblyVersion("3.5.2.*")]
+[assembly: AssemblyInformationalVersion("3.5.2")]
